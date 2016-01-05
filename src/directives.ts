d9d207a2904f2ecd8fccccec90867ab4233a8c20
Richard Natal <Bigous@users.noreply.github.com>
2016-01-05 17:25:24 -0200

    Added Ng2Highcharts directive

--- src/directives.ts
+++ src/directives.ts
@@ -1,12 +1,12 @@
 import {CONST_EXPR} from 'angular2/src/facade/lang';
 
 // Import all directives
-import {SampleDirective} from './directives/sample.directive';
+import {Ng2Highcharts} from './directives/ng2-highcharts';
 
 // Export all directives
-export {SampleDirective} from './directives/sample.directive';
+export {Ng2Highcharts} from './directives/ng2-highcharts';
 
 // Export convenience property
 export const DIRECTIVES = CONST_EXPR([
-  SampleDirective
+	Ng2Highcharts
 ]);
