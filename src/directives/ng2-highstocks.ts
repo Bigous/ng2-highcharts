0bd5b40e31e5d04ed9dfcc486931aa8f4a0c5013
Dino O <dosmanovic@devlogic.eu>
2016-02-08 09:55:11 +0100

    ident fix

--- src/directives/ng2-highstocks.ts
+++ src/directives/ng2-highstocks.ts
@@ -2,33 +2,33 @@
 import {Directive, ElementRef, Input} from 'angular2/core';
 
 @Directive({
-	selector: '[ng2-highstocks]'
+    selector: '[ng2-highstocks]'
 })
 export class Ng2Highstocks {
-	hostElement: ElementRef;
-	chart: HighchartsChartObject;
-	constructor(ele: ElementRef) {
-		this.hostElement = ele;
-	}
+    hostElement: ElementRef;
+    chart: HighchartsChartObject;
+    constructor(ele: ElementRef) {
+        this.hostElement = ele;
+    }
 
-	@Input('ng2-highstocks') set options(opt:HighchartsOptions) {
-		if(!opt) {
-			console.log('No valid options...');
-			console.log(opt);
-			return;
-		}
-		if(opt.series || opt.data) {
-			if(this.chart) {
-				this.chart.destroy();
-			}
+    @Input('ng2-highstocks') set options(opt: HighchartsOptions) {
+        if (!opt) {
+            console.log('No valid options...');
+            console.log(opt);
+            return;
+        }
+        if (opt.series || opt.data) {
+            if (this.chart) {
+                this.chart.destroy();
+            }
             if (!opt.chart) {
-				opt.chart = {};
-			}
-			opt.chart.renderTo = this.hostElement.nativeElement;
-			this.chart = new Highcharts.StockChart(opt);
-		} else {
-			console.log('No valid options...');
-			console.dir(opt);
-		}
-	}
+                opt.chart = {};
+            }
+            opt.chart.renderTo = this.hostElement.nativeElement;
+            this.chart = new Highcharts.StockChart(opt);
+        } else {
+            console.log('No valid options...');
+            console.dir(opt);
+        }
+    }
 }
