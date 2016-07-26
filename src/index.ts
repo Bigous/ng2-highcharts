cf18fcc643abac1fa0d909b3ac840f5da37ead35
Richard Natal <bigous@gmail.com>
2016-07-26 13:26:26 -0300

    Chore: Adequacoes para rodar com ionic2

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * from './ng2-highcharts';
-export * from './ng2-highstocks';
-export * from './ng2-highmaps';
+export * from './directives/ng2-highcharts';
+export * from './directives/ng2-highstocks';
+export * from './directives/ng2-highmaps';
